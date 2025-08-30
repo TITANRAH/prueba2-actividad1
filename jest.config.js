37c739938d3517138560dbd5e9411fc0bd3b9e40
TITANRAH <granrah1@gmail.com>
2025-08-29 21:33:22 -0400

    funciona

--- jest.config.js
+++ jest.config.js
@@ -1,2 +1,9 @@
-/** @type {import("jest").Config} */
-module.exports={preset:"ts-jest",testEnvironment:"node",moduleNameMapper:{"^@/(.*)$":"<rootDir>/$1"},coverageDirectory:"coverage",coverageReporters:["text","lcov","html"],reporters:["default",["jest-junit",{outputDirectory:"reports/junit",outputName:"junit.xml"}]],};
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: { '^@/(.*)$': '<rootDir>/$1' },
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text','lcov','html'],
+  reporters: ['default', ['jest-junit', { outputDirectory: 'reports/junit', outputName: 'junit.xml' }]],
+};
